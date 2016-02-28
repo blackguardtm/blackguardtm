f852319e5bb7d5f3ddaa298c5f0a7145be730beb
morteza b.guard <gmorteza143@gmail.com>
2016-02-28 17:25:54 +0330

    Create xy.lua

---
+++ plugins/xy.lua
@@ -0,0 +1,23 @@
+do
+local function run(msg, matches)
+local x = 132667916
+local y = 169925264
+local z = 144728365
+    if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(x) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
+    end
+ elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(z) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..z, ok_cb, false)
+    end
+ elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(y) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..y, ok_cb, false)
+    end
+end
+ 
+return {
+  patterns = {
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
